c27b7800cbce1285a17d648ddbd853a347b902e8
rsvts <rysiavetsandrei@gmail.com>
2019-10-23 16:12:12 +0200

    task completed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,87 @@
 class TicTacToe {
+
     constructor() {
 
+        this.x = 'x';
+        this.o = 'o';
+        this.current = 'x';
+        this.winner = null;
+        this.gameField = Array(3).fill().map(() => Array(3).fill(null));
     }
 
     getCurrentPlayerSymbol() {
 
+        return this.current;
     }
 
-    nextTurn(rowIndex, columnIndex) {
+    nextTurn(row, col) {
 
+        if (!this.gameField[row][col]) {
+
+            this.gameField[row][col] = this.getCurrentPlayerSymbol();
+
+            this.current = (this.current == this.o) ? this.x : this.o;
+
+        }
+
+        return true;
     }
 
     isFinished() {
 
+        return this.noMoreTurns() || !!this.getWinner();
     }
 
     getWinner() {
+
+        for (let i = 0; i < this.gameField.length; i++) {
+
+            if (this.gameField[i][0] === this.gameField[i][1] &&
+                this.gameField[i][0] === this.gameField[i][2]) {
+
+                return this.winner = this.gameField[i][0];
+            }
+
+            if (this.gameField[0][i] === this.gameField[1][i] &&
+                this.gameField[0][i] === this.gameField[2][i]) {
+
+                return this.winner = this.gameField[0][i];
+
+            }
+
+        }
+
+        if (this.gameField[0][0] === this.gameField[1][1] &&
+            this.gameField[0][0] === this.gameField[2][2]) {
+
+            return this.winner = this.gameField[0][0];
+        }
+
+
+        if (this.gameField[2][0] === this.gameField[1][1] &&
+            this.gameField[2][0] === this.gameField[0][2]) {
+
+            return this.winner = this.gameField[2][0];
+        }
+
+        return this.winner;
 
     }
 
     noMoreTurns() {
 
+        return !this.gameField.find((item) => item.includes(null));
     }
 
     isDraw() {
 
+        return this.isFinished() && !this.getWinner();
+
     }
 
-    getFieldValue(rowIndex, colIndex) {
+    getFieldValue(row, col) {
 
+        return this.gameField[row][col];
     }
 }
 
